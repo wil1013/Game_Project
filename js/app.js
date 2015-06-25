79096b28aef383d69e64022deb6b3a57bc330a80
Wil Olitan <wil.j.olitan@gmail.com>
2015-06-25 14:46:09 -0400

    updated with game checker

--- js/app.js
+++ js/app.js
@@ -1,20 +1,39 @@
 $(function() {
 	console.log('Loaded');
+$('#1').click(gameChecker);
 });
+var winningCombo = ['RRRR','BBBB'];
 
+
+var totalMoves = function() {
+	return nextOpen1+nextOpen2+nextOpen3+nextOpen4+nextOpen5+nextOpen6+nextOpen7;
+}
+
+var tied = function(){
+	if(totalMoves()==42){
+		alert('Tied');
+	}
+};
+var checkForWin = function(columnNumber){
+ 	var jointString = columnNumber.join('');
+ 	if(jointString.includes(winningCombo[0]) || jointString.includes(winningCombo[1])){
+ 		alert('game over');
+ 	}
+}
+
+
+var gameChecker =function(){
+	checkForWin(bottom1Col);
+}
 
 /*
-var move = function(element){
-		var slot =  
-		if($('div button').text())== "") {
-
-			if(turn ===0){
-				element.target.innerText = 'x';
-				turn = 1;
-			} else if (turn ===1) {
-				element.target.innerText = 'o';
-				turn = 0;
-			};
-		};
-}
+var bottom1Col =[];
+var bottom2Col =[];
+var bottom3Col =[];
+var bottom4Col =[];
+var bottom5Col =[];
+var bottom6Col =[];
+var bottom7Col =[];
 */
+
+
--- js/space.js
+++ js/space.js
@@ -1,7 +1,12 @@
 $(function() {
 	console.log('Loaded');
 $('#1').click(makeColorCol1);
-$('#2').click(makeColor2);
+$('#2').click(makeColorCol2);
+$('#3').click(makeColorCol3);
+$('#4').click(makeColorCol4);
+$('#5').click(makeColorCol5);
+$('#6').click(makeColorCol6);
+$('#7').click(makeColorCol7);
 
 });
 
@@ -18,31 +23,137 @@ var nextOpen5 = 0;
 var nextOpen6 = 0;
 var nextOpen7 = 0;
 
+var colOrder = ['f','e','d','c','b','a'];
+
+var bottom1Col =[];
+var bottom2Col =[];
+var bottom3Col =[];
+var bottom4Col =[];
+var bottom5Col =[];
+var bottom6Col =[];
+var bottom7Col =[];
+
 
 var makeColorCol1 = function(){
-var bottomACol =[];
-
-var colOrder = ['f','e','d','c','b','a'];	
-	if(bottomACol.length>=colHeight){	
+	if(bottom1Col.length>=colHeight){	
 	}
 	else if(turn == 0){
-	//var nextFill= $('#col1 div')[colHeight-bottomACol.length];
-	//var newClass='<div id="rowa" class="red"></div>';
-	//nextFill=newClass;
 	$(this).children('#'+colOrder[parseInt(nextOpen1)]).attr('class','red');
 	
-	var child = $(this).children()[1];//want to change class of specific child within index of array, but doesn't work on child??
+	//-works- var child = $(this).children().eq(1);//want to change class of specific child within index of array, but doesn't work on child??
 	turn=1;
-	bottomACol.push('R');
+	bottom1Col.push('R');
 	nextOpen1++;	
 	}
 
 	else if(turn == 1){
 	$(this).children('#'+colOrder[parseInt(nextOpen1)]).attr('class','black');
-	//$(this).attr('class','black');
 	turn=0;
-	bottomACol.push('B');
+	bottom1Col.push('B');
 	nextOpen1++;
+	}
+
+};
+var makeColorCol2 = function(){
+	if(bottom2Col.length>=colHeight){	
+	}
+	else if(turn == 0){
+	$(this).children('#'+colOrder[parseInt(nextOpen2)]).attr('class','red');
+	turn=1;
+	bottom2Col.push('R');
+	nextOpen2++;	
+	}
+	else if(turn == 1){
+	$(this).children('#'+colOrder[parseInt(nextOpen2)]).attr('class','black');
+	turn=0;
+	bottom2Col.push('B');
+	nextOpen2++;
+	}
+
+};
+
+var makeColorCol3 = function(){
+	if(bottom3Col.length>=colHeight){	
+	}
+	else if(turn == 0){
+	$(this).children('#'+colOrder[parseInt(nextOpen3)]).attr('class','red');
+	turn=1;
+	bottom3Col.push('R');
+	nextOpen3++;	
+	}
+	else if(turn == 1){
+	$(this).children('#'+colOrder[parseInt(nextOpen3)]).attr('class','black');
+	turn=0;
+	bottom3Col.push('B');
+	nextOpen3++;
+	}
+
+};
+var makeColorCol4 = function(){
+	if(bottom4Col.length>=colHeight){	
+	}
+	else if(turn == 0){
+	$(this).children('#'+colOrder[parseInt(nextOpen4)]).attr('class','red');
+	turn=1;
+	bottom4Col.push('R');
+	nextOpen4++;	
+	}
+	else if(turn == 1){
+	$(this).children('#'+colOrder[parseInt(nextOpen4)]).attr('class','black');
+	turn=0;
+	bottom4Col.push('B');
+	nextOpen4++;
+	}
+
+};
+var makeColorCol5 = function(){
+	if(bottom5Col.length>=colHeight){	
+	}
+	else if(turn == 0){
+	$(this).children('#'+colOrder[parseInt(nextOpen5)]).attr('class','red');
+	turn=1;
+	bottom5Col.push('R');
+	nextOpen5++;	
+	}
+	else if(turn == 1){
+	$(this).children('#'+colOrder[parseInt(nextOpen5)]).attr('class','black');
+	turn=0;
+	bottom5Col.push('B');
+	nextOpen5++;
+	}
+
+};
+var makeColorCol6 = function(){
+	if(bottom6Col.length>=colHeight){	
+	}
+	else if(turn == 0){
+	$(this).children('#'+colOrder[parseInt(nextOpen6)]).attr('class','red');
+	turn=1;
+	bottom6Col.push('R');
+	nextOpen6++;	
+	}
+	else if(turn == 1){
+	$(this).children('#'+colOrder[parseInt(nextOpen6)]).attr('class','black');
+	turn=0;
+	bottom6Col.push('B');
+	nextOpen6++;
+	}
+
+};
+var makeColorCol7 = function(){
+	if(bottom7Col.length>=colHeight){	
+	}
+	else if(turn == 0){
+	$(this).children('#'+colOrder[parseInt(nextOpen7)]).attr('class','red');
+	turn=1;
+	bottom7Col.push('R');
+	nextOpen7++;	
+	}
+	else if(turn == 1){
+	$(this).children('#'+colOrder[parseInt(nextOpen7)]).attr('class','black');
+	turn=0;
+	bottom7Col.push('B');
+	nextOpen7++;
 	}
 
 };
